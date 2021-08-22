94d87b416ad151ed0e2c36f9e3aea4cc4508768d
shahreaz <ashahreaz@gmail.com>
2021-08-23 03:02:30 +0600

    Add color weight to each color in the pallete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Palette from "./components/Palette";
 import colorPalettes from "./utils/seedColors";
+import generateNewPalette from "./utils/generateColorShade.js";
+
 function App() {
+  console.log(generateNewPalette(colorPalettes[1]));
   return (
     <div className="App">
       <Palette colorPalette={colorPalettes[1]} />
---
+++ src/utils/generateColorShade.js
@@ -0,0 +1,67 @@
+import chroma from "chroma-js";
+
+const colors = {
+	paletteName: "Material UI Colors",
+	id: "material-ui-colors",
+	emoji: "🎨",
+	colors: [
+		{ name: "red", color: "#F44336" },
+		{ name: "pink", color: "#E91E63" },
+		{ name: "purple", color: "#9C27B0" },
+		{ name: "deeppurple", color: "#673AB7" },
+		{ name: "indigo", color: "#3F51B5" },
+		{ name: "blue", color: "#2196F3" },
+		{ name: "lightblue", color: "#03A9F4" },
+		{ name: "cyan", color: "#00BCD4" },
+		{ name: "teal", color: "#009688" },
+		{ name: "green", color: "#4CAF50" },
+		{ name: "lightgreen", color: "#8BC34A" },
+		{ name: "lime", color: "#CDDC39" },
+		{ name: "yellow", color: "#FFEB3B" },
+		{ name: "amber", color: "#FFC107" },
+		{ name: "orange", color: "#FF9800" },
+		{ name: "deeporange", color: "#FF5722" },
+		{ name: "brown", color: "#795548" },
+		{ name: "grey", color: "#9E9E9E" },
+		{ name: "bluegrey", color: "#607D8B" },
+	],
+};
+
+const levels = [50, 100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+const generateNewPalette = (starterPallete) => {
+	const newPalette = {
+		paletteName: starterPallete.paletteName,
+		id: starterPallete.id,
+		emoji: starterPallete.emoji,
+		colors: {},
+	};
+
+	levels.forEach((level) => (newPalette.colors[level] = []));
+
+	starterPallete.colors.forEach((color, index) => {
+		const scale = getColorScale(color.color, 10).reverse();
+
+		scale.forEach((hex, index) => {
+			newPalette.colors[levels[index]].push({
+				name: `${color.name} ${levels[index]}`,
+				id: color.name.toLowerCase().replace(/ /g, "-"),
+				hex: hex,
+				rgb: chroma(hex).css(),
+				rgba: chroma(hex).alpha(0.9999).css(),
+			});
+		});
+	});
+
+	return newPalette;
+};
+
+const getRange = (color) => {
+	return [chroma(color).darken(1.4), color, "#ffffff"];
+};
+
+const getColorScale = (color, numberOfGeneratedColor) => {
+	return chroma.scale(getRange(color)).colors(numberOfGeneratedColor);
+};
+
+export default generateNewPalette;
